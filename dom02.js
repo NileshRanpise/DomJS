e9cef6435c47272cbfe1afced9cc1c426c7863ae
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-02-07 19:59:15 +0530

    newjs commit

---
+++ dom02.js
@@ -0,0 +1,77 @@
+// Traversing the DOM
+
+let itemList = document.querySelector('#items');
+
+// Parent Node
+
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'green'
+
+// Parent element
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'green';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// child node
+
+// console.log(itemList.childNodes);
+// console.log(itemList.childNodes[1]);
+// itemList.childNodes[1].style.backgroundColor = 'red';
+
+//1st child
+
+// console.log(itemList.firstChild);
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello';
+
+// last child
+
+// console.log(itemList.lastChild);
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello';
+
+// nextsibling
+
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+// itemList.nextElementSibling.textContent = 'Hello';
+
+// previousSibling
+
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.textContent = 'Hello';
+
+// create Element
+
+// let newEle = document.createElement('div');
+// newEle.className = 'hello';
+// newEle.id = 'id01';
+
+// newEle.setAttribute('title','Hello div');
+
+// let newCele = document.createTextNode('Hello World');
+// newEle.appendChild(newCele)
+// console.log(newEle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
